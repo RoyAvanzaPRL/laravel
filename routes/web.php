43cf313798f15ee52bda9915e804b09d64de285c
RoyAvanzaPRL <roy@avanzaprl.es>
2026-09-08 08:51:41 +0200

    blank laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('contacts', ContactController::class);
